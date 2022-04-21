01970174148bbc477236ed304a9845b6ca469667
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-21 11:57:05 +0300

    do a deck

--- dyrak.cpp
+++ dyrak.cpp
@@ -7,13 +7,12 @@ using namespace std;
 
 int main() {
     srand(time(NULL));
-    vector<vector<string> > card_deck;
+    vector<vector<string> > card_deck(13, vector<string>(4));
     string suits[4] = {"Чірва", "Піка", "Буба", "Креста"};
     string cards[13] = {"Два", "Три", "Чотири", "П'ять", "Шість", "Сім", "Вісім", "Дев'ять", "Десять", "Валет", "Дама", "Король", "Туз"};
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 12; j++) {
-            card_deck[i][j] = suits[rand() % 4] + cards[rand() % 13];
-            cout << card_deck[i][j] << endl;
+            card_deck[i][j] = cards[rand() % 13] + " " + suits[rand() % 4];
         }
     }
 }
