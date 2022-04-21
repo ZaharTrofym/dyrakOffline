24f8b58c469b7ae50fc6a1362e5468d5c8cea9f9
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-21 12:50:25 +0300

    Add class card deck and voit get deck

--- dyrak.cpp
+++ dyrak.cpp
@@ -5,9 +5,19 @@
 
 using namespace std;
 
+class Card_deck {
+    private:
+        vector<string> deck;
+    public:
+        void getDeck(vector<string> deck) {
+            this->deck = deck;
+        }
+};
+
 int main() {
     srand(time(NULL));
-    
+
+    Card_deck deckk;
     vector<string> card_deck(52);
     string suits[4] = {"Чірва", "Піка", "Буба", "Креста"};
     string cards[13] = {"Два", "Три", "Чотири", "П'ять", "Шість", "Сім", "Вісім", "Дев'ять", "Десять", "Валет", "Дама", "Король", "Туз"};
@@ -15,7 +25,7 @@ int main() {
     for (int i = 0; i < card_deck.size(); i++) {
         card_deck[i] = cards[rand() % 13] + " " + suits[rand() % 4];
     }
-
+    deckk.getDeck(card_deck);
     vector<string> player1(6);
     vector<string> player2(6);
 
