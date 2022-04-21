1f0fba4221a4b7415e22cff9d7084a833e172103
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-21 12:42:47 +0300

    Added players Fill cards for player1

--- dyrak.cpp
+++ dyrak.cpp
@@ -7,12 +7,20 @@ using namespace std;
 
 int main() {
     srand(time(NULL));
-    vector<vector<string> > card_deck(13, vector<string>(4));
+    
+    vector<string> card_deck(52);
     string suits[4] = {"Чірва", "Піка", "Буба", "Креста"};
     string cards[13] = {"Два", "Три", "Чотири", "П'ять", "Шість", "Сім", "Вісім", "Дев'ять", "Десять", "Валет", "Дама", "Король", "Туз"};
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 12; j++) {
-            card_deck[i][j] = cards[rand() % 13] + " " + suits[rand() % 4];
-        }
+    
+    for (int i = 0; i < card_deck.size(); i++) {
+        card_deck[i] = cards[rand() % 13] + " " + suits[rand() % 4];
+    }
+
+    vector<string> player1(6);
+    vector<string> player2(6);
+
+    for (int i = 0; i < 6; i++) {
+        player1[i] = card_deck[card_deck.size() - 1];
+        card_deck.pop_back();
     }
 }
