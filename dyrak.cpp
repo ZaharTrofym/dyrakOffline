af03c2d474925888f5d44fb3c19e1b142f204a27
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-05-11 21:40:39 +0300

    Add sorting and remove error

--- dyrak.cpp
+++ dyrak.cpp
@@ -65,6 +65,10 @@ class Player {
             cards.pop_back();
         }
 
+        void setCards(vector<Card*> cards) {
+            this->cards = cards;
+        }
+
         vector<Card*> getCards() {
             return cards;
         }
@@ -72,35 +76,61 @@ class Player {
         Card* getCard(int num) {
             return cards[num - 1];
         }
+
+        vector<Card*> sorting(vector<Card*> cards) {
+            vector<Card*> emptyErray;
+            int count = cards.size();
+
+            for (int i = 0; i < count; i++) {
+                int check = 0;
+                int value = 0;
+
+                for (int j = 0; j < cards.size(); j++) {
+                    if (j == 0) {
+                        value = cards[0]->getType() * 100 + cards[0]->getNominal();
+                    }
+                    
+                    if (value > cards[j]->getType() * 100 + cards[j]->getNominal()) {
+                        value = cards[j]->getType() * 100 + cards[j]->getNominal();
+                        check = j;
+                    }
+                }
+
+                emptyErray.push_back(cards[check]);
+                cards.erase(cards.begin() + check);
+            }
+
+            return emptyErray;
+        }
 };
 
 class Deck {
     private:
         vector<Card*> deck;
-        vector<Card*> shuffle_deck;
+        vector<Card*> shuffleDeck;
     public:
         void addCardToDeck(Card* card) {
             deck.push_back(card);
         }
 
         void addCardToShuffle(Card* card) {
-            shuffle_deck.push_back(card);
+            shuffleDeck.push_back(card);
         }
 
         void showCards() {
             string card;
-            for(int i = 0; i < shuffle_deck.size(); i++) {
-                card = shuffle_deck[i]->getName();
+            for(int i = 0; i < shuffleDeck.size(); i++) {
+                card = shuffleDeck[i]->getName();
                 cout << card << endl;
             }
         }
 
-        void setShuffleDeck(vector<Card*> shuffle_deck) {
-            this->shuffle_deck = shuffle_deck;
+        void setShuffleDeck(vector<Card*> shuffleDeck) {
+            this->shuffleDeck = shuffleDeck;
         }
         
         vector<Card*> getShuffleDeck() {
-            return shuffle_deck;
+            return shuffleDeck;
         }
 
         Card* getRandomCardInDeck() {
@@ -108,8 +138,8 @@ class Deck {
         }
 
         Card* getCardInShuffleDeck() {
-            Card* card = shuffle_deck[shuffle_deck.size() - 1];
-            shuffle_deck.pop_back();
+            Card* card = shuffleDeck[shuffleDeck.size() - 1];
+            shuffleDeck.pop_back();
             
             return card;
         }
@@ -130,7 +160,7 @@ class Engine {
 
         Deck* generateDeck() {
             Deck* deck = new Deck();
-            vector<Card*> shuffle_deck;
+            vector<Card*> shuffleDeck;
 
             for (int i = 0; i < 4; i++) {
                 if (i + 1 == root) {
@@ -151,17 +181,17 @@ class Engine {
             while(i < 52) {
                 card = deck->getRandomCardInDeck();
                 inDeck = false;
-                for (int i = 0; i < shuffle_deck.size(); i++) {
-                    if (card == shuffle_deck[i]) {
+                for (int i = 0; i < shuffleDeck.size(); i++) {
+                    if (card == shuffleDeck[i]) {
                         inDeck = true;
                     }
                 }
                 if (inDeck == false) {
-                    shuffle_deck.push_back(card);
+                    shuffleDeck.push_back(card);
                     i++;
                 }
             }
-            deck->setShuffleDeck(shuffle_deck);
+            deck->setShuffleDeck(shuffleDeck);
 
             return deck;
         }
@@ -207,31 +237,20 @@ class Engine {
             vector<Card*> walker;
             
             if (walk == 1) {  //хід гравця
-                if (deck->getShuffleDeck().size() > 0) {  //відповідає за заповнення карт гравців
-                    if (player1->getCards().size() < player2->getCards().size()) {
-                        for (int i = 0; i < 6 - player1->getCards().size(); i++) {
-                            if (deck->getShuffleDeck().size() != 0 && player1->getCards().size() < 6) {
-                                player1->addCard(deck->getCardInShuffleDeck());
-                            }
-                            if (deck->getShuffleDeck().size() != 0 && player2->getCards().size() < 6) {
-                                player2->addCard(deck->getCardInShuffleDeck());
-                            }
+                if (deck->getShuffleDeck().size() > 0) {
+                    for (int i = 0; i < 6; i++) {
+                        if (player2->getCards().size() < 6) {
+                            player2->addCard(deck->getCardInShuffleDeck());
                         }
-                    }
 
-                    if (player2->getCards().size() < player1->getCards().size()) {
-                        for (int i = 0; i < 6 - player2->getCards().size(); i++) {
-                            if (deck->getShuffleDeck().size() != 0 && player2->getCards().size() < 6) {
-                                player2->addCard(deck->getCardInShuffleDeck());
-                            }
-                            if (deck->getShuffleDeck().size() != 0 && player1->getCards().size() < 6) {
-                                player1->addCard(deck->getCardInShuffleDeck());
-                            }
+                        if (player1->getCards().size() < 6) {
+                            player1->addCard(deck->getCardInShuffleDeck());
                         }
                     }
                 }
 
                 showRoot();  
+                player1->setCards(player1->sorting(player1->getCards()));
                 player1->showCardsForWalk(suits);
                 cin >> num1;
 
@@ -295,6 +314,19 @@ class Engine {
             else if (walk == 2) {  //хід суперника
                 vector<Card*> temp;
                 int take;
+
+                if (deck->getShuffleDeck().size() > 0) {
+                    for (int i = 0; i < 6; i++) {
+                        if (player1->getCards().size() < 6) {
+                            player1->addCard(deck->getCardInShuffleDeck());
+                        }
+
+                        if (player2->getCards().size() < 6) {
+                            player2->addCard(deck->getCardInShuffleDeck());
+                        }
+                    }
+                }
+
                 if (deck->getShuffleDeck().size() > 0) {  //додавання карт якщо їх не вистачає
                     if (player1->getCards().size() < player2->getCards().size()) {
                         for (int i = 0; i < 6 - player1->getCards().size(); i++) {
@@ -348,7 +380,9 @@ class Engine {
                     return true;
                 }
 
+                player1->setCards(player1->sorting(player1->getCards()));
                 showRoot();  
+
                 for (int i = 0; i < player1->getCards().size(); i++) {  //пошук карт які можуть відбити атаку
                     if (walker[0]->getRoot() == false) {  //якщо атака не козирь
                         if (player1->getCard(i + 1)->getType() == walker[0]->getType() && player1->getCard(i + 1)->getNominal() > walker[0]->getNominal()) {
@@ -377,14 +411,14 @@ class Engine {
                 else {
                     cout << "\nТи не можеш відбити атаку, тому забираєш карти\n";
                     takeCards = true;
-                    player1->getCards().push_back(walker[0]);
+                    player1->addCard(walker[0]);
 
                     return false;
                 }
                 if (num2 == take) {
                     cout << "\nТи вирішив взяти карти\n";
                     takeCards = true;
-                    player1->getCards().push_back(walker[0]);
+                    player1->addCard(walker[0]);
                     
                     return false;
                 }
@@ -402,7 +436,7 @@ class Engine {
                 else {
                     cout << "\nТи ввів не ту цифру, тому щоб не було помилки ти взяв карти\n";
                     takeCards = true;
-                    player1->getCards().push_back(walker[0]);
+                    player1->addCard(walker[0]);
                     
                     return false;
                 }
@@ -433,7 +467,7 @@ int main() {
                 cout << "\nХід суперника";
             }
             pause();
-            engine->oneRound(player1, player2, deck);
+            isEnd = engine->oneRound(player1, player2, deck);
 
             if (engine->getWalk() == 1 && engine->getTakeCards() == false) {
                 engine->setWalk(2);
@@ -441,12 +475,6 @@ int main() {
             else if (engine->getWalk() == 2 && engine->getTakeCards() == false) {
                 engine->setWalk(1);
             }
-            // if (engine->getWalk() == 1 && engine->getTakeCards() == false) {
-            //     engine->setWalk(2);
-            // }
-            // else if (engine->getWalk() == 1 && engine->getTakeCards() == false) {
-            //     engine->setWalk(1);
-            // }
     }
 
     pause("\nкінець\n");
